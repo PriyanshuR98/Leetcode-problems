17f32aa92e0c653ef28999cd32b018dce3e791d0
Priyanshu Rawat <114494873+PriyanshuR98@users.noreply.github.com>
2023-08-09 19:43:59 +0530

    Time: 138 ms (89.04%), Space: 81.1 MB (38.62%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+        // trying nlogn solution.
+    
+            // binary searching on answer..
+    bool checker(int possdiff,int p, vector<int>&nums)
+    {
+        int cnt=0;
+        int i=0;
+        int n=nums.size();
+        
+        while(i<=n-2)
+        {
+            if(nums[i+1]-nums[i]<=possdiff)
+            {
+                cnt++;
+                i+=2;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return (cnt>=p ) ? 1 : 0;
+    }
+
+
+    int minimizeMax(vector<int>& nums, int p) {
+
+        sort(nums.begin(),nums.end());
+
+        int low=0;
+        int high=nums[nums.size()-1]-nums[0];
+
+        while(high-low>1)
+        {
+            int mid= (low+high)/2;
+
+            if(checker(mid,p,nums)==true)
+            {
+                high=mid;
+            }
+            else
+            {
+                low=mid;
+            }
+        }
+
+
+        if(checker(low,p,nums)==true)
+        {
+            return low;
+        }
+        return high;
+
+
+        
+    }
+};
