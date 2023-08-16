1ac475f42265c442ff8fa030a41a9f6437a9e44f
Priyanshu Rawat <114494873+PriyanshuR98@users.noreply.github.com>
2023-08-16 10:04:48 +0530

    Time: 293 ms (66.41%), Space: 132.7 MB (63.70%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    int helper(int index, vector<int>&prices, bool brought, int transactions,vector<vector<vector<int> > > &dp)
-    {
-        if(index>=prices.size())
+        int solver(vector<int>&prices, int index, int transactions ,  vector<vector<int>>&dp)
         {
-            return 0;
+            if(index==prices.size() or transactions>=4)
+            {
+                return 0;
+            }
+            
+            if(dp[index][transactions]!=-1)
+            {
+                return dp[index][transactions];
+            }
+            
+            int profit=0;
+            if(transactions%2==0)  // can buy
+            {
+                
+                profit=max(-prices[index]+solver(prices,index+1,transactions+1,dp)  , solver(prices,index+1,transactions,dp) );  
+            }
+            else  // can sell
+            {
+                profit=max(+prices[index]+solver(prices,index+1,transactions+1,dp)  , solver(prices,index+1,transactions,dp) );  
+            }
+            
+            return dp[index][transactions]= profit;
         }
-        if(transactions>2)
-        {
-            return 0;
-        }
-        if(dp[index][brought][transactions]!=-1)
-        {
-            return dp[index][brought][transactions];
-        }
-        int profit=0;
-
-        if(brought==0)
-        {
-            // if(transactions<2)  // we can buy
-            // {
-                profit= -prices[index]+helper(index+1,prices,1,transactions+1,dp);
-            // }
-
-            // we dont want to buy
-            profit=max(profit, helper(index+1,prices,0, transactions,dp));
-        }
-        else  // we cans sell
-        {
-            profit= prices[index]+helper(index+1,prices,0,transactions,dp);
-            profit=max(profit,helper(index+1,prices,1,transactions,dp));
-        }
-
-        return dp[index][brought][transactions]= profit;
-    }
-
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<vector<int> > > dp(n, vector<vector<int> >(2, vector<int>(3, -1)));
         
-        return helper(0,prices,0,0,dp);
+        // b s b s ..  0 1 2 3
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        
+        return solver(prices,0,0,dp);
+        
+        
         
     }
 };
