f0cd6f727219f658401a155a304f9f9af63f0971
Priyanshu Rawat <114494873+PriyanshuR98@users.noreply.github.com>
2023-08-16 10:32:31 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (52.75%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    int helper(int index, vector<int>prices, bool bought , vector<vector<int>>&dp)
-    {
-        if(index>=prices.size())
-        {
-            return 0;
-        }
-        
-        if(dp[index][bought]!=-1)
-              {
-                  return dp[index][bought];
-              }
-        int profit=0;
-        
-        if(bought==0)  //can buy
-        {
-            profit=max(-prices[index]+helper(index+1,prices,1,dp) ,  helper(index+1,prices,0,dp));
-        }
-        
-        else
-        {
-             profit=max(+prices[index]+helper(index+2,prices,0,dp) ,  helper(index+1,prices,1,dp));
-        }
-        
-        return dp[index][bought]=profit;
-    
-    }
     int maxProfit(vector<int>& prices) {
         
-    int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-       return helper(0,prices,0,dp);
+        
+         int n=prices.size();
+        vector<vector<int>>dp(n+2,vector<int>(2,-1));
+      
+        dp[n][0]=0;
+        dp[n][1]=0;
+        dp[n+1][0]=0;
+        dp[n+1][1]=0;
+        
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int bought=0;bought<=1;bought++)
+            {
+                 int profit=0;
+        
+                    if(bought==0)  //can buy
+                    {
+                        profit=max(-prices[index]+dp[index+1][1] ,  dp[index+1][0]);
+                    }
+
+                    else
+                    {
+                         profit=max(+prices[index]+dp[index+2][0] ,  dp[index+1][1]);
+                    }
+
+                   dp[index][bought]=profit;
+            }
+        }
+        
+        return dp[0][0];
         
     }
 };
